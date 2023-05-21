03b6bd266d2e3a53ca1788afa80f817bd6fd2064
Izaias Rios <izaias.rios@gmail.com>
2023-05-21 20:26:21 -0300

    Inicio

---
+++ LanchesMac/Migrations/20230514130637_PopularCategorias.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchesMac.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categorias(CategoriaNome, Descricao)" +
+                                 " VALUES('Normal','Lanches feitos com ingredientes tradicionais')");
+
+            migrationBuilder.Sql("INSERT INTO Categorias(CategoriaNome, Descricao)" +
+                                 " VALUES('Naturais','Lanches feitos com ingredientes integrais e naturais')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categorias");
+        }
+    }
+}
