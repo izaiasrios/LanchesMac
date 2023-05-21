f5bd1ad5ba2b8c2658b5a63000b8eef42687a342
Izaias Rios <izaias.rios@gmail.com>
2023-05-21 17:20:30 -0300

    Criando a partial view _LanchesResumo

---
+++ LanchesMac/Controllers/LancheController.cs
@@ -0,0 +1,37 @@
+﻿using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+
+            
+        {
+            var lanchesListViewModel = new LancheListViewModel();
+            lanchesListViewModel.Lanches = _lancheRepository.Lanches;
+            lanchesListViewModel.CategoriaAtual = "Categoria Atual";
+
+            return View(lanchesListViewModel);
+
+            //ViewData["Título"] = "Todos os Lanches";
+            //ViewData["Data"] = DateTime.Now;
+
+            //var lanches = _lancheRepository.Lanches;
+            //var totalLanches = lanches.Count();
+
+            //ViewBag.TotalTitulo = "Total de lanches :";
+            //ViewBag.TotalLanches = totalLanches;
+            //return View(lanches);
+        }
+    }
+}
